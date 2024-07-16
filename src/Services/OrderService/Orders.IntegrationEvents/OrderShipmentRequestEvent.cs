342a925c3d87a45cdc31160ff3a7eb82a2d19e86
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-07-16 14:15:21 +0500

    Added anaytics integration event

--- src/Services/OrderService/Orders.IntegrationEvents/OrderShipmentRequestEvent.cs
+++ src/Services/OrderService/Orders.IntegrationEvents/OrderShipmentRequestEvent.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Orders.IntegrationEvents;
+﻿namespace Orders.IntegrationEvents;
 
 public sealed record OrderShipmentRequestEvent(
     Guid OrderId,
     Guid CustomerId,
     ShippingAddress Address,
-    OrderItem Item
+    List<OrderItem> Item
     );

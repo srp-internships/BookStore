d221874d520c4a9b7e8854bba4dd913ba168dd05
faridun <user@WIN-3QQ6D95ILTD>
2024-07-16 13:22:22 +0500

    Created Shipment mass transit event

---
+++ src/Services/ShipmentService/ShipmentService.IntegrationEvent/ShipmentProcces.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShipmentService.IntegrationEvent
+{
+    public class ShipmentProcces
+    {
+        [Key]
+        public Guid ShipmentId { get; set; }
+        public ShipmentStatus Stats { get; set; }
+        public DateTime estimatedDeliveryDate { get; set; }
+        public string Message { get; set; }
+
+    }
+}

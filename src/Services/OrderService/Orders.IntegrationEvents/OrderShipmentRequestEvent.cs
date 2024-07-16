c7fff1903bf108af44e198fdb4bbd38039fae6e7
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-07-16 13:51:30 +0500

    Added shipment integration event

---
+++ src/Services/OrderService/Orders.IntegrationEvents/OrderShipmentRequestEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.IntegrationEvents;
+
+public sealed record OrderShipmentRequestEvent(
+    Guid OrderId,
+    Guid CustomerId,
+    ShippingAddress Address,
+    OrderItem Item
+    );

d444c55341f0031e380da8bb34e8ccf34eab3177
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-07-17 10:14:39 +0500

    Added database provider

--- src/Services/OrderService/OrderService.Infrastructure/Persistence/DataBases/ApplicationDbContext.cs
+++ src/Services/OrderService/OrderService.Infrastructure/Persistence/DataBases/ApplicationDbContext.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
 
-namespace OrderService.Infrastructure.Persistence.DataBases
+namespace OrderService.Infrastructure.Persistence.DataBases;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+    public DbSet<Order> Orders { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=OrderService;Username=postgres;Password=7878_Postgresql");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder); 
     }
 }

b2ecce9712565d118e28a63a3cae3ef3d65c27f9
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-07-18 13:00:40 +0500

    Added interceptors

---
+++ src/Services/OrderService/OrderService.Application/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+using OrderService.Application.Behaviors;
+using System.Reflection;
+
+namespace OrderService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        services.AddFeatureManagement();
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
+
---
+++ src/Services/OrderService/OrderService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿namespace OrderService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        // Add services to the container.
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
+}

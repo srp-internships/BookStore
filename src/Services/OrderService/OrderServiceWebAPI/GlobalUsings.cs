0e426aad11a08a0a2d3ceacc8d1b31e737c1c873
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-07-18 15:43:08 +0500

    Added order commands

--- src/Services/OrderService/OrderServiceWebAPI/GlobalUsings.cs
+++ src/Services/OrderService/OrderServiceWebAPI/GlobalUsings.cs
@@ -1,1 +1,12 @@
-﻿
+﻿global using Carter;
+global using MediatR;
+global using OrderService.Application.Dtos;
+global using OrderService.Application.Orders.Commands.CreateOrder;
+global using Mapster;
+global using Microsoft.EntityFrameworkCore;
+global using OrderServiceWebAPI;
+global using OrderService.Application;
+global using OrderService.Infrastructure;
+global using OrderService.Infrastructure.Persistence.DataBases.Extensions;
+global using OrderService.Infrastructure.Persistence.DataBases;
+
--- src/Services/OrderService/OrderServiceWebAPI/Program.cs
+++ src/Services/OrderService/OrderServiceWebAPI/Program.cs
@@ -1,31 +1,34 @@
-using Microsoft.EntityFrameworkCore;
-using OrderService.Infrastructure.Persistence.DataBases;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//// Configure the HTTP request pipeline.
+app.UseApiServices();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    await app.InitialiseDatabaseAsync();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();

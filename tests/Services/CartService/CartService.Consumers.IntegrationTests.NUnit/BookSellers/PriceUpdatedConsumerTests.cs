eb8f90e6677a4dedfbea4d6fa21f1e45d331d13f
faridun <user@WIN-3QQ6D95ILTD>
2024-07-23 08:54:54 +0500

    Created  integration tests BookUpdatedEvent and PriceUpdated Event

--- tests/Services/CartService/CartService.Consumers.IntegrationTests.NUnit/BookSellers/PriceUpdatedConsumerTests.cs
+++ tests/Services/CartService/CartService.Consumers.IntegrationTests.NUnit/BookSellers/PriceUpdatedConsumerTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CartService.Consumers.BookSellers;
+using CartService.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,64 @@ namespace CartService.Consumers.IntegrationTests.NUnit.BookSellers
 {
     public class PriceUpdatedConsumerTests
     {
+        private CartDbContext _context;
+        private Mock<ILogger<PriceUpdatedConsumer>> _loggerMock;
+        private PriceUpdatedConsumer _consumer;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<CartDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new CartDbContext(options);
+
+            _loggerMock = new Mock<ILogger<PriceUpdatedConsumer>>();
+            _consumer = new PriceUpdatedConsumer(_loggerMock.Object, _context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task Consume_PriceUpdatedEvent_ShouldUpdatePriceInDatabase()
+        {
+            // Arrange
+            var existingPrice = new BookSeller
+            {
+                BookId = Guid.NewGuid(),
+                SellerId = Guid.NewGuid(),
+                Price = 10.0m
+            };
+
+            _context.BookSellers.Add(existingPrice);
+            await _context.SaveChangesAsync();
+
+            var priceUpdatedEvent = new PriceUpdatedEvent
+            {
+                BookId = existingPrice.BookId,
+                SellerId = existingPrice.SellerId,
+                Price = 20.0m
+            };
+
+            var consumeContextMock = new Mock<ConsumeContext<PriceUpdatedEvent>>();
+            consumeContextMock.Setup(x => x.Message).Returns(priceUpdatedEvent);
+
+            // Act
+            await _consumer.Consume(consumeContextMock.Object);
+
+            // Assert
+            var updatedPrice = await _context.BookSellers.FirstOrDefaultAsync(
+                p => p.BookId == priceUpdatedEvent.BookId &&
+                     p.SellerId == priceUpdatedEvent.SellerId);
+            Assert.That(updatedPrice, Is.Not.Null);
+            Assert.That(updatedPrice.Price, Is.EqualTo(priceUpdatedEvent.Price));
+        }
     }
 }
+
--- tests/Services/CartService/CartService.Consumers.IntegrationTests.NUnit/Books/BookUpdatedConsumerTests.cs
+++ tests/Services/CartService/CartService.Consumers.IntegrationTests.NUnit/Books/BookUpdatedConsumerTests.cs
@@ -1,17 +1,66 @@
+using CartService.Consumers.Books;
+using CartService.Domain.Entities;
+
 namespace CartService.Consumers.IntegrationTests.NUnit
 {
     public class BookUpdatedConsumerTests
-
     {
+        private CartDbContext _context;
+        private Mock<ILogger<BookUpdatedConsumer>> _loggerMock;
+        private BookUpdatedConsumer _consumer;
+
         [SetUp]
         public void Setup()
         {
+            var options = new DbContextOptionsBuilder<CartDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new CartDbContext(options);
+
+            _loggerMock = new Mock<ILogger<BookUpdatedConsumer>>();
+            _consumer = new BookUpdatedConsumer(_loggerMock.Object, _context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
         }
 
         [Test]
-        public void Test1()
+        public async Task Consume_BookUpdatedEvent_ShouldUpdateBookInDatabase()
         {
-            Assert.Pass();
+            // Arrange
+            var existingBook = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Original Title",
+                Image = "OriginalImage.png"
+            };
+
+            _context.Books.Add(existingBook);
+            await _context.SaveChangesAsync();
+
+            var bookUpdatedEvent = new BookUpdatedEvent
+            {
+                Id = existingBook.Id,
+                Title = "Updated Title",
+                Image = "UpdatedImage.png"
+            };
+
+            var consumeContextMock = new Mock<ConsumeContext<BookUpdatedEvent>>();
+            consumeContextMock.Setup(x => x.Message).Returns(bookUpdatedEvent);
+
+            // Act
+            await _consumer.Consume(consumeContextMock.Object);
+
+            // Assert
+            var book = await _context.Books.FindAsync(bookUpdatedEvent.Id);
+            Assert.That(book, Is.Not.Null);
+            Assert.That(book.Title, Is.EqualTo(bookUpdatedEvent.Title));
+            Assert.That(book.Image, Is.EqualTo(bookUpdatedEvent.Image));
         }
     }
 }

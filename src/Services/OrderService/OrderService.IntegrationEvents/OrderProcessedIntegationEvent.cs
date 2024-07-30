18f1d454429240b7c722c5f071e3539227b44d15
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-07-30 12:31:52 +0500

    order service contracts syncronized

---
+++ src/Services/OrderService/OrderService.IntegrationEvents/OrderProcessedIntegationEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace OrderService.IntegrationEvents;
+
+public record OrderProcessedIntegrationEvent(Guid OrderId, Guid CustomerId, Address ShippingAddress,
+	OrderStatus Status, List<OrderItem> Items, decimal TotalPrice);
+
+public record Address(string FirstName, string LastName, string EmailAddress, string Country, string State, string Street);
+
+public record OrderItem(Guid BookId, Guid SellerId, int Quantity, decimal Price);
+
+
+public enum OrderStatus
+{
+	Completed,
+	Failed,
+	PaymentProcessing,
+	ShipmentProcessing
+}
+

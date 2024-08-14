54afbaf124f432ee78ccfc1b14c6122cf5d9ac92
faridun <user@WIN-3QQ6D95ILTD>
2024-08-14 17:27:24 +0500

    added Secure Microservices Using JWT With Ocelot

--- src/Services/ApiGetAway/ApiGateway/Program.cs
+++ src/Services/ApiGetAway/ApiGateway/Program.cs
@@ -2,14 +2,13 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOcelot();
+
 
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
-builder.Configuration.AddJsonFile("ocelot.json");
-
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+builder.Services.AddAuthentication();
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseAuthorization();
@@ -17,6 +16,8 @@ app.UseSwaggerForOcelotUI(option =>
 {
     option.PathToSwaggerGenerator = "/swagger/docs";
 });
+app.UseAuthentication();
+app.UseAuthorization();
 await app.UseOcelot();
 
 app.Run();

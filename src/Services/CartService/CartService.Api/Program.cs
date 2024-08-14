f3b772d48386e0fe970252725d27710a4e1ab6eb
faridun <user@WIN-3QQ6D95ILTD>
2024-08-14 15:49:29 +0500

    Change Configurations Mass transit

--- src/Services/CartService/CartService.Api/Program.cs
+++ src/Services/CartService/CartService.Api/Program.cs
@@ -29,31 +29,7 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host("rabbitmq://localhost", h =>
-        {
-            h.Username("guest");
-            h.Password("guest");
-        });
-
-        cfg.ReceiveEndpoint("book-created-event-queue", e =>
-        {
-            e.ConfigureConsumer<BookCreatedConsumer>(context);
-        });
-
-        cfg.ReceiveEndpoint("price-created-event-queue", e =>
-        {
-            e.ConfigureConsumer<PriceCreatedConsumer>(context);
-        });
-
-        cfg.ReceiveEndpoint("book-updated-event-queue", e =>
-        {
-            e.ConfigureConsumer<BookUpdatedConsumer>(context);
-        });
-
-        cfg.ReceiveEndpoint("price-updated-event-queue", e =>
-        {
-            e.ConfigureConsumer<PriceUpdatedConsumer>(context);
-        });
+        cfg.ConfigureEndpoints(context);
     });
 });
 #endregion
